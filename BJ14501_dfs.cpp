bd081e26374a7f441f29edc8e6356140f8522263
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-05 12:30:23 +0900

    Update BJ14501_dfs.cpp

--- BJ14501_dfs.cpp
+++ BJ14501_dfs.cpp
@@ -12,25 +12,22 @@ int T[15];
 int P[15];
 int money = 0;
 
-void print_tp(){
-    for(int i=0; i<N; i++) cout << T[i] << " " << P[i] << endl;
-}
-
 void consult(int i, int pay, int day){
-    cout << "now idx = " << i << ", pay = " << pay << ", day = " << day << endl;
     if(i >= N){
-        if(pay > money) money = pay;
-        cout << "finish money = " << money << endl;
+        if(pay > money){
+            money = pay;
+        }
         return;
     }
-    if((day+T[i]<=N) && (i+1+T[i]<=N+1)) consult(T[i]+i, pay+P[i], day+T[i]);
+    if(day+T[i]<=N && i+1+T[i]<=N+1) consult(i+T[i], pay+P[i], day+T[i]);
     consult(i+1, pay, day);
-    return;
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N;
-    for(int i=0; i<N; i++) cin >> T[i] >> P[i];
+    for(int i=0; i<N; i++) {
+        cin >> T[i] >> P[i];
+    }
     consult(0, 0, 0);
     cout << money << endl;
     return 0;
