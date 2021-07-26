ff606aee7ada91d8e518d32e9eff8c3220f6d78c
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-07-27 02:47:44 +0900

    Update BJ14502.cpp

--- BJ14502.cpp
+++ BJ14502.cpp
@@ -2,72 +2,74 @@
 //  main.cpp
 //  BJ14502
 //
-//  Created by Hwayeon on 2021/01/09.
+//  Created by Hwayeon on 2021/07/27.
 //
+
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
 
-int N,M;
-int map[8][8] = {1,};
-int map_copy[8][8] = {0,};
+int N, M;
+int map[8][8] = {0,};
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
-int max_safe_area = 0;
-struct location{
-    int x;
-    int y;
-};
-location loc;
-vector<location> virus;
+int cmp_map[8][8] = {0,};
+int answer = 0;
+vector<pair<int, int>> virus;
+
+void copy_map(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cmp_map[i][j] = map[i][j];
+        }
+    }
+}
 
 void spread_virus(){
-    queue<location> q;
-    int safe_area = 0;
+    queue<pair<int, int>> q;
     for(int i=0; i<virus.size(); i++){
         q.push(virus[i]);
     }
     while(!q.empty()){
-        int v_x = q.front().x;
-        int v_y = q.front().y;
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
-        for(int d=0; d<4; d++){
-            int new_x = v_x+dx[d];
-            int new_y = v_y+dy[d];
-            if(new_x<0 || new_x>=N || new_y<0 || new_y>=M) continue;
-            if(map_copy[new_x][new_y] == 0){
-                map_copy[new_x][new_y] = 2;
-                loc.x = new_x;
-                loc.y = new_y;
-                q.push(loc);
+        for(int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx<0 || nx>=M || ny<0 || ny>=N) continue;
+            if(cmp_map[ny][nx] == 0){
+                cmp_map[ny][nx] = 2;
+                q.push(make_pair(nx, ny));
             }
         }
     }
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(map_copy[i][j]==0) safe_area++;
-        }
-    }
-    if(safe_area > max_safe_area) max_safe_area = safe_area;
-    return;
 }
 
-void install_wall(int wall){
-    if(wall == 3){
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                map_copy[i][j] = map[i][j];
-            }
+void count_safe_area(){
+    int safe_area = 0;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(cmp_map[i][j] == 0) safe_area++;
         }
+    }
+    if(answer < safe_area) answer = safe_area;
+}
+
+void build_wall(int cnt){
+    if(cnt == 3){
+        //spread virus
+        copy_map();
         spread_virus();
+        count_safe_area();
         return;
     }
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             if(map[i][j] == 0){
                 map[i][j] = 1;
-                install_wall(wall+1);
+                build_wall(cnt+1);
                 map[i][j] = 0;
             }
         }
@@ -79,16 +81,12 @@ int main(int argc, const char * argv[]) {
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin >> map[i][j];
-            if(map[i][j] == 0) continue;
-            else if(map[i][j] == 2){
-                loc.x = i;
-                loc.y = j;
-                virus.push_back(loc);
+            if(map[i][j] == 2){
+                virus.push_back(make_pair(j, i));
             }
         }
     }
-    install_wall(0);
-    cout << max_safe_area <<endl;
-    
+    build_wall(0);
+    cout << answer << endl;
     return 0;
 }
