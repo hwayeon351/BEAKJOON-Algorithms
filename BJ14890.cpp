3c80468afa826f7488d775f187c54cd2f6a9f01d
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-09-19 13:37:54 +0900

    Update BJ14890.cpp

--- BJ14890.cpp
+++ BJ14890.cpp
@@ -2,135 +2,164 @@
 //  main.cpp
 //  BJ14890
 //
-//  Created by Hwayeon on 2021/01/12.
+//  Created by Hwayeon on 2021/09/18.
 //
+
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <cmath>
+#include <math.h>
+#include <string.h>
 using namespace std;
 
-int road[100][100];
+int map[100][100] = {0, };
+int visit[100] = {0, };
+int road = 0;
 int N, L;
 
-int find_road_row(){
-    vector<pair<int,int> > q;
-    int row_road = 0;
-    for(int row=0; row<N; row++){
-        q.clear();
-        bool visit[100] = {false,};
-        q.push_back(make_pair(row, 0));
-        int col = 1;
-        while(col < N){
-            if(road[row][col] == road[q.back().first][q.back().second]){
-                q.push_back(make_pair(row, col));
-                col++;
+void check_road(){
+    for(int r=0; r<N; r++){
+        memset(visit, 0, sizeof(visit));
+        int check = true;
+        int before = map[r][0];
+        int cnt = 1;
+        int c = 1;
+        while(c < N){
+            if(map[r][c] == before){
+                cnt ++;
+                c ++;
+                continue;
             }
-            else if(road[row][col] > road[q.back().first][q.back().second]){
-                if(abs(road[row][col] - road[q.back().first][q.back().second]) == 1){
-                    if(q.size()>=L){
-                        bool check = true;
-                        for(int i=1; i<=L; i++){
-                            int now_col = q[q.size()-i].second;
-                            if(visit[now_col]==true) check = false;
-                        }
-                        if(check == false) break;
-                        else{
-                            q.clear();
-                            q.push_back(make_pair(row, col));
-                            col++;
-                        }
-                    }
-                    else break;
-                }
-                else break;
+            //높이 차이가 2 이상인 경우, 길이 아니다
+            else if(abs(map[r][c] - before) > 1){
+                check = false;
+                break;
             }
+            //높이 차이가 1인 경우,
             else{
-                if(abs(road[row][col] - road[q.back().first][q.back().second]) == 1){
-                    bool check = true;
-                    for(int i=col; i<col+L; i++){
-                        if(road[row][i] != road[row][col]) check = false;
+                //오르막길 인 경우,
+                if(map[r][c] - before == 1){
+                    //경사로를 놓을 수 있는 경우,
+                    if(cnt >= L){
+                        for(int cc=c-L; cc<c; cc++){
+                            //이미 경사로가 설치되어 있는 경우, 설치 불가 -> 길이 아니다
+                            if(visit[cc]){
+                                check = false;
+                                break;
+                            }
+                            visit[cc] = 1;
+                        }
+                        //경사로 설치 완료
+                        before = map[r][c];
+                        cnt = 1;
+                        c ++;
+                        continue;
                     }
-                    if(check == false) break;
+                    //경사로를 놓을 수 없는 경우, 길이 아니다
                     else{
-                        q.clear();
-                        for(int i=col; i<col+L; i++){
-                            visit[i] = true;
-                        }
-                        q.push_back(make_pair(row, col+L-1));
-                        col = col+L;
+                        check = false;
+                        break;
                     }
                 }
-                else break;
-            }
-        }
-        if(col == N) {
-            row_road++;
-        }
-    }
-    return row_road;
-}
-
-int find_road_col(){
-    int col_road = 0;
-    vector<pair<int,int> > q;
-    for(int col=0; col<N; col++){
-        q.clear();
-        bool visit[100] = {false,};
-        q.push_back(make_pair(0, col));
-        int row = 1;
-        while(row < N){
-            if(road[row][col] == road[q.back().first][q.back().second]){
-                q.push_back(make_pair(row, col));
-                row++;
-            }
-            else if(road[row][col] > road[q.back().first][q.back().second]){
-                if(abs(road[row][col] - road[q.back().first][q.back().second]) == 1){
-                    if(q.size()>=L){
-                        bool check = true;
-                        for(int i=1; i<=L; i++){
-                            int now_row = q[q.size()-i].first;
-                            if(visit[now_row]==true) check = false;
-                        }
-                        if(check == false) break;
-                        q.clear();
-                        q.push_back(make_pair(row, col));
-                        row++;
-                    }
-                    else break;
-                }
-                else break;
-            }
-            else{
-                bool check = true;
-                for(int i=row; i<row+L; i++){
-                    if(road[i][col] != road[row][col]) check = false;
-                }
-                if(check == false) break;
+                //내리막길 인 경우,
                 else{
-                    q.clear();
-                    for(int i=row; i<row+L; i++){
-                        visit[i] = true;
+                    int cc;
+                    for(cc=c+1; cc<c+L; cc++){
+                        if(map[r][cc] == map[r][c]) continue;
+                        check = false;
+                        break;
                     }
-                    q.push_back(make_pair(row+L-1, col));
-                    row = row+L;
+                    if(!check) break;
+                    //경사로를 놓을 수 있는 경우, 경사로 설치
+                    for(cc=c; cc<c+L; cc++){
+                        visit[cc] = 1;
+                    }
+                    before = map[r][c];
+                    cnt = L;
+                    c++;
+                    continue;
                 }
             }
         }
-        if(row == N) {
-            col_road++;
+        if(check) {
+            road++;
         }
     }
-    return col_road;
+    for(int c=0; c<N; c++){
+        memset(visit, 0, sizeof(visit));
+        int check = true;
+        int before = map[0][c];
+        int cnt = 1;
+        int r = 1;
+        while(r < N){
+            if(map[r][c] == before){
+                cnt ++;
+                r ++;
+                continue;
+            }
+            //높이 차이가 2 이상인 경우, 길이 아니다
+            else if(abs(map[r][c] - before) > 1){
+                check = false;
+                break;
+            }
+            //높이 차이가 1인 경우,
+            else{
+                //오르막길 인 경우,
+                if(map[r][c] - before == 1){
+                    //경사로를 놓을 수 있는 경우,
+                    if(cnt >= L){
+                        for(int rr=r-L; rr<r; rr++){
+                            //이미 경사로가 설치되어 있는 경우, 설치 불가 -> 길이 아니다
+                            if(visit[rr]){
+                                check = false;
+                                break;
+                            }
+                            visit[rr] = 1;
+                        }
+                        //경사로 설치 완료
+                        before = map[r][c];
+                        cnt = 1;
+                        r ++;
+                        continue;
+                    }
+                    //경사로를 놓을 수 없는 경우, 길이 아니다
+                    else{
+                        check = false;
+                        break;
+                    }
+                }
+                //내리막길 인 경우,
+                else{
+                    int rr;
+                    for(rr=r+1; rr<r+L; rr++){
+                        if(map[rr][c] == map[r][c]) continue;
+                        check = false;
+                        break;
+                    }
+                    if(!check) break;
+                    //경사로를 놓을 수 있는 경우, 경사로 설치
+                    for(rr=r; rr<r+L; rr++){
+                        visit[rr] = 1;
+                    }
+                    before = map[r][c];
+                    cnt = L;
+                    r++;
+                    continue;
+                }
+            }
+        }
+        if(check) {
+            road++;
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N >> L;
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
-            cin >> road[i][j];
+            cin >> map[i][j];
         }
     }
-    cout << find_road_row()+find_road_col() << endl;
+    check_road();
+    cout << road << endl;
     return 0;
 }
