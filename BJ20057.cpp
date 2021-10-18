37fa00e6b7d508d907e0c8fd29895cbcdc9d4afd
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-10-18 14:35:34 +0900

    Update BJ20057.cpp

--- BJ20057.cpp
+++ BJ20057.cpp
@@ -1,288 +1,81 @@
 //
 //  main.cpp
-//  BJ20057
+//  BJ20057_1
 //
-//  Created by Hwayeon on 2021/09/16.
+//  Created by Hwayeon on 2021/10/18.
 //
 
 #include <iostream>
 using namespace std;
 
 int N;
-int board[500][500] = {0, };
-int dc[4] = {-1, 0, 1, 0};
-int dr[4] = {0, 1, 0, -1};
-long long total_out = 0;
+int A[500][500] = {0, };
 
-void spread_sand(int r, int c, int d){
-    int sum = 0;
-    int per = 0;
-    int out = 0;
-    int y = board[r][c];
-    switch (d) {
-        //왼쪽
-        case 0:
-            per = y*0.02;
-            if(r-2 >= 0){
-                board[r-2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+2 < N) {
-                board[r+2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.07;
-            if(r-1 >= 0){
-                board[r-1][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N){
-                board[r+1][c] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.01;
-            if(r-1 >= 0 && c+1 < N){
-                board[r-1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c+1 < N){
-                board[r+1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.1;
-            if(r-1 >= 0 && c-1 >= 0){
-                board[r-1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c-1 >= 0){
-                per = y*0.1;
-                board[r+1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.05;
-            if(c-2 >= 0){
-                board[r][c-2] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c-1 >= 0) board[r][c-1] += (board[r][c] - (sum + out));
-            else out += (board[r][c] - (sum + out));
-            board[r][c] = 0;
-            total_out += out;
-            break;
-        //아래쪽
-        case 1:
-            per = y*0.02;
-            if(c-2 >= 0){
-                board[r][c-2] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c+2 < N) {
-                board[r][c+2] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.07;
-            if(c-1 >= 0){
-                board[r][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c+1 < N){
-                board[r][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.01;
-            if(r-1 >= 0 && c+1 < N){
-                board[r-1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r-1 >= 0 && c-1 >= 0){
-                board[r-1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.1;
-            if(r+1 < N && c-1 >= 0){
-                board[r+1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c+1 < N){
-                board[r+1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.05;
-            if(r+2 < N){
-                board[r+2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N) board[r+1][c] += (board[r][c] - (sum + out));
-            else out += (board[r][c] - (sum + out));
-            board[r][c] = 0;
-            total_out += out;
-            break;
-        //오른쪽
-        case 2:
-            per = y*0.02;
-            if(r-2 >= 0){
-                board[r-2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+2 < N) {
-                board[r+2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.07;
-            if(r-1 >= 0){
-                board[r-1][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N){
-                board[r+1][c] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.01;
-            if(r-1 >= 0 && c-1 >= 0){
-                board[r-1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c-1 >= 0){
-                board[r+1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.1;
-            if(r-1 >= 0 && c+1 < N){
-                board[r-1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c+1 < N){
-                board[r+1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.05;
-            if(c+2 < N){
-                board[r][c+2] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c+1 < N) board[r][c+1] += (board[r][c] - (sum + out));
-            else out += (board[r][c] - (sum + out));
-            board[r][c] = 0;
-            total_out += out;
-            break;
-        //위쪽
-        case 3:
-            per = y*0.02;
-            if(c-2 >= 0){
-                board[r][c-2] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c+2 < N) {
-                board[r][c+2] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.07;
-            if(c-1 >= 0){
-                board[r][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(c+1 < N){
-                board[r][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.01;
-            if(r+1 < N && c+1 < N){
-                board[r+1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r+1 < N && c-1 >= 0){
-                board[r+1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.1;
-            if(r-1 >= 0 && c-1 >= 0){
-                board[r-1][c-1] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r-1 >= 0 && c+1 < N){
-                board[r-1][c+1] += per;
-                sum += per;
-            }
-            else out += per;
-            per = y*0.05;
-            if(r-2 >= 0){
-                board[r-2][c] += per;
-                sum += per;
-            }
-            else out += per;
-            if(r-1 >= 0) board[r-1][c] += (board[r][c] - (sum + out));
-            else out += (board[r][c] - (sum + out));
-            board[r][c] = 0;
-            total_out += out;
-            break;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int spread[4][10][3] = {
+    //0-좌
+    {{0,-2,2}, {-1,-1,10}, {0,-1,7}, {1,-1,1}, {-2,0,5}, {-1,1,10}, {0,1,7}, {1,1,1}, {0,2,2}, {-1, 0, 0}},
+    //1-하
+    {{-1,-1,1}, {1,-1,1}, {-2,0,2}, {-1,0,7}, {1,0,7}, {2,0,2}, {-1,1,10}, {1,1,10}, {0,2,5}, {0,1,0}},
+    //2-우
+    {{0,-2,2}, {-1,-1,1}, {0,-1,7}, {1,-1,10}, {2,0,5}, {-1,1,1}, {0,1,7}, {1,1,10}, {0,2,2}, {1,0,0}},
+    //3-상
+    {{0,-2,5}, {-1,-1,10}, {1,-1,10}, {-2,0,2}, {-1,0,7}, {1,0,7}, {2,0,2}, {-1,1,1}, {1,1,1}, {0,-1,0}}
+};
+
+int tx, ty;
+int gone = 0;
+
+void spread_sand(int tx, int ty, int d){
+    int ay = A[ty][tx];
+    int total_spread = 0;
+    A[ty][tx] = 0;
+    int x, y, p, sand;
+    for(int i=0; i<9; i++){
+        x = tx + spread[d][i][0];
+        y = ty + spread[d][i][1];
+        p = spread[d][i][2];
+        sand = ay*(0.01*p);
+        if(x<0 || x>=N || y<0 || y>=N) gone += sand;
+        else A[y][x] += sand;
+        total_spread += sand;
     }
+    x = tx + spread[d][9][0];
+    y = ty + spread[d][9][1];
+    if(x<0 || x>=N || y<0 || y>=N) gone += (ay-total_spread);
+    else A[y][x] += (ay-total_spread);
 }
 
 void move_tornado(){
-    int tc = N/2;
-    int tr = N/2;
-    int dir = 0;
-    for(int i=1; i<=N; i++){
-        for(int j=0; j<2; j++){
-            for(int k=1; k<=i; k++){
-                int yr = tr+dr[dir];
-                int yc = tc+dc[dir];
-                tr = yr;
-                tc = yc;
-                if(i==N && k==N-1){
-                    spread_sand(yr, yc, dir);
-                    cout << total_out <<endl;
-                    return;
-                }
-                if(board[yr][yc] == 0) continue;
-                spread_sand(yr, yc, dir);
+    int d = 0;
+    for(int dis=1; dis<=N; dis++){
+        int ndis = dis;
+        for(int i=0; i<2; i++){
+            if(dis == N) ndis = N-1;
+            for(int j=0; j<ndis; j++){
+                tx += dx[d];
+                ty += dy[d];
+                spread_sand(tx, ty, d);
             }
-            dir = (dir+1) % 4;
+            if(dis == N) return;
+            d = (d+1)%4;
         }
     }
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cin >> board[i][j];
+    for(int y=0; y<N; y++){
+        for(int x=0; x<N; x++){
+            cin >> A[y][x];
         }
     }
+    tx = N/2;
+    ty = N/2;
     move_tornado();
+    cout << gone << endl;
+    
     return 0;
 }
