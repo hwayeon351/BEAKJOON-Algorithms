714fcf5057cfbe96b896af7cb99e4bc5927895cb
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-10-21 16:19:17 +0900

    Update BJ19238.cpp

--- BJ19238.cpp
+++ BJ19238.cpp
@@ -1,8 +1,8 @@
 //
 //  main.cpp
-//  BJ19238
+//  BJ19238_1
 //
-//  Created by Hwayeon on 2021/09/14.
+//  Created by Hwayeon on 2021/10/21.
 //
 
 #include <iostream>
@@ -10,122 +10,147 @@
 #include <string.h>
 using namespace std;
 
-int N, M, gas;
-int area[21][21] = {0, };
-int visit[21][21] = {0, };
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, -1, 0, 1};
+int N, M;
+long long gas;
+int area[22][22] = {0, };
+int visit[22][22] = {0, };
 int texi_x, texi_y;
 
 struct Customer{
+    int num;
     int start_x;
     int start_y;
     int dest_x;
     int dest_y;
-    int dist_texi;
-    int dist_dest;
+    int dis = 0;
+    bool done = false;
 };
-Customer customer;
 struct cmp{
     bool operator()(Customer c1, Customer c2){
-        if(c1.dist_texi == c2.dist_texi){
+        if(c1.dis == c2.dis){
             if(c1.start_y == c2.start_y){
                 return c1.start_x > c2.start_x;
             }
             return c1.start_y > c2.start_y;
         }
-        return c1.dist_texi > c2.dist_texi;
+        return c1.dis > c2.dis;
     }
 };
+Customer cus;
+Customer customers[401];
 
-priority_queue<Customer, vector<Customer>, cmp> customers;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
 
-int get_dist(int start_x, int start_y, int dest_x, int dest_y){
-    int dist = -1;
-    memset(visit, 0, sizeof(visit));
-    visit[start_y][start_x] = 1;
-    queue<pair<pair<int, int>, int>> q;
-    q.push(make_pair(make_pair(start_x, start_y), 0));
-    while(!q.empty()){
-        int now_x = q.front().first.first;
-        int now_y = q.front().first.second;
-        int now_dist = q.front().second;
-        q.pop();
-        if(now_y == dest_y && now_x == dest_x){
-            dist = now_dist;
-            break;
-        }
-        for(int d=0; d<4; d++){
-            int nx = now_x + dx[d];
-            int ny = now_y + dy[d];
-            if(nx<1 || nx>N || ny<1 || ny>N) continue;
-            if(visit[ny][nx] == 1) continue;
-            if(area[ny][nx] == 1) continue;
-            q.push({{nx, ny}, now_dist+1});
-            visit[ny][nx] = 1;
+int cnt = 0;
+
+Customer find_customer(){
+    priority_queue<Customer, vector<Customer>, cmp> pq;
+    for(int i=0; i<M; i++){
+        if(customers[i].done) continue;
+        cus = customers[i];
+        memset(visit, 0, sizeof(visit));
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{texi_x, texi_y}, 0});
+        visit[texi_y][texi_x] = 1;
+        while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int dis = q.front().second;
+            q.pop();
+            if(x==cus.start_x && y==cus.start_y){
+                cus.dis = dis;
+                pq.push(cus);
+                break;
+            }
+            for(int d=0; d<4; d++){
+                int nx = x+dx[d];
+                int ny = y+dy[d];
+                if(nx<1 || nx>N || ny<1 || ny>N) continue;
+                if(area[ny][nx]) continue;
+                if(visit[ny][nx]) continue;
+                visit[ny][nx] = 1;
+                q.push({{nx, ny}, dis+1});
+            }
         }
     }
-    return dist;
+    if(pq.empty()){
+        cus.num = -1;
+        return cus;
+    }
+    return pq.top();
 }
 
-
-int main(int argc, const char * argv[]) {
-    cin >> N >> M >> gas;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            cin >> area[i][j];
-        }
-    }
-    cin >> texi_y >> texi_x;
-    bool check = true;
-    for(int i=0; i<M; i++){
-        cin >> customer.start_y >> customer.start_x >> customer.dest_y >> customer.dest_x;
-        customer.dist_texi = get_dist(texi_x, texi_y, customer.start_x, customer.start_y);
-        customer.dist_dest = get_dist(customer.start_x, customer.start_y, customer.dest_x, customer.dest_y);
-        if(customer.dist_texi == -1 || customer.dist_dest == -1){
-            check = false;
-            continue;
-        }
-        customers.push(customer);
-    }
-    
-    if(!check){
-        cout << -1 << endl;
-        return 0;
-    }
-
-    while(!customers.empty()){
-        //1. 승객 태우기
-        customer = customers.top();
-        customers.pop();
-        texi_x = customer.start_x;
-        texi_y = customer.start_y;
-        gas -= customer.dist_texi;
-        if(gas <= 0){
+void start_texi(){
+    while(cnt < M){
+        cus = find_customer();
+        if(cus.num == -1){
             gas = -1;
             break;
         }
-        
-        //2. 목적지로 가기
-        texi_x = customer.dest_x;
-        texi_y = customer.dest_y;
-        gas -= customer.dist_dest;
+        //1. 손님 태우러 이동
+        gas -= cus.dis;
         if(gas < 0){
             gas = -1;
             break;
         }
-        gas += (2*customer.dist_dest);
+        texi_x = cus.start_x;
+        texi_y = cus.start_y;
         
-        //3. 승객 우선순위 재정렬
-        priority_queue<Customer, vector<Customer>, cmp> copy_customers;
-        while(!customers.empty()){
-            customer = customers.top();
-            customers.pop();
-            customer.dist_texi = get_dist(texi_x, texi_y, customer.start_x, customer.start_y);
-            copy_customers.push(customer);
+        //2. 목적지로 이동
+        memset(visit, 0, sizeof(visit));
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{texi_x, texi_y}, 0});
+        visit[texi_y][texi_x] = 1;
+        int dest_dis = 0;
+        while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int dis = q.front().second;
+            q.pop();
+            if(x==cus.dest_x && y==cus.dest_y){
+                gas -= dis;
+                dest_dis = dis;
+                texi_x = x;
+                texi_y = y;
+                customers[cus.num].done = true;
+                break;
+            }
+            for(int d=0; d<4; d++){
+                int nx = x+dx[d];
+                int ny = y+dy[d];
+                if(nx<1 || nx>N || ny<1 || ny>N) continue;
+                if(area[ny][nx]) continue;
+                if(visit[ny][nx]) continue;
+                visit[ny][nx] = 1;
+                q.push({{nx, ny}, dis+1});
+            }
         }
-        customers = copy_customers;
+        if(dest_dis == 0 || gas < 0){
+            gas = -1;
+            break;
+        }
+        cnt++;
+        gas += (2*dest_dis);
     }
+}
+
+int main(int argc, const char * argv[]) {
+    cin >> N >> M >> gas;
+    for(int y=1; y<=N; y++){
+        for(int x=1; x<=N; x++){
+            cin >> area[y][x];
+        }
+    }
+    cin >> texi_y >> texi_x;
+    for(int i=0; i<M; i++){
+        cin >> cus.start_y >> cus.start_x >> cus.dest_y >> cus.dest_x;
+        cus.num = i;
+        customers[i] = cus;
+    }
+    
+    start_texi();
     cout << gas << endl;
+    
     return 0;
 }
