ccb0460a90242506412725752643f74ae2ba9a45
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-01-24 19:48:17 +0900

    Update BJ2217.py

--- BJ2217.py
+++ BJ2217.py
@@ -1,11 +1,9 @@
 import sys
 def get_maximum_weight(N, rope):
     weight = rope[0]
-    cnt = 1
     for i in range(1, N):
         if rope[i]*(i+1) > weight:
             weight = rope[i]*(i+1)
-            cnt += 1
     print(weight)
 
 input = sys.stdin.readline
