688f73096e1313dea3267fda956c358c53189c11
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-01-26 12:18:15 +0900

    Update BJ1931.py

--- BJ1931.py
+++ BJ1931.py
@@ -4,12 +4,9 @@ def get_maximum_cnt(N, meetings):
     end = meetings[0][1]
     cnt = 1
     for i in range(1, N):
-        #이전 회의가 i번째 회의보다 늦게 끝나는 경우, i번째 회의를 선택한다
         if end > meetings[i][1]:
             start = meetings[i][0]
             end = meetings[i][1]
-        #이전 회의가 i번째 회의보다 빨리 끝나는 경우,
-        #이전 회의가 끝난 이후 i번째 회의가 시작되는 경우, i번째 회의를 선택한다
         elif end <= meetings[i][0]:
             cnt += 1
             start = meetings[i][0]
