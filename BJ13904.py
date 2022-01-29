0b3d6c92669be96d794091a526589892d588184a
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-01-29 17:03:19 +0900

    Update BJ13904.py

--- BJ13904.py
+++ BJ13904.py
@@ -1,20 +1,25 @@
+import sys
+def get_max_score(homework, N):
+    day = homework[0][0]
+    visit = [0]*N
+    score = 0
+    while day > 0:
+        i = -1
+        for j in range(N):
+            if visit[j]: continue
+            if homework[j][0] >= day:
+                if i == -1: i = j
+                elif homework[j][1] > homework[i][1]: i = j
+        if i != -1:
+            visit[i] = 1
+            score += homework[i][1]
+        day -= 1
+    print(score)
+
+input = sys.stdin.readline
 N = int(input())
-dw = [[0,0] for _ in range(N)]
-visit = [0]*N
-for i in range(N):
-    dw[i] = list(map(int, input().split()))
-dw.sort(reverse = True)
-timer = dw[0][0]
-total = 0
-while timer > 0:
-    idx = -1
-    for i in range(N):
-        if dw[i][0] < timer: break
-        if visit[i] == 0:
-            if idx == -1: idx = i
-            elif dw[idx][1] < dw[i][1]: idx = i
-    if idx != -1:
-        visit[idx] = 1
-        total += dw[idx][1]
-    timer-=1
-print(total)
+homework = list()
+for _ in range(N):
+    homework.append(list(map(int, input().split())))
+homework.sort(reverse = True)
+get_max_score(homework, N)
