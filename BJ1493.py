36bf68dac27768e02538df5195c3ba9f53a65656
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-01-31 00:59:45 +0900

    Update BJ1493.py

--- BJ1493.py
+++ BJ1493.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 def get_cube_cnt(l, w, h, N, cube):
     box = [[l, w, h]]
     cnt = 0
@@ -6,12 +6,16 @@ def get_cube_cnt(l, w, h, N, cube):
         L, W, H = box.pop()
         if L == 0 or W == 0 or H == 0: continue
         for i in range(N-1, -1, -1):
-            d = 2**cube[i][0]
-            if d <= L and d <= W and d <= H and cube[i][1] > 0:
-                cube_cnt = min(L//d, W//d, H//d, cube[i][1])
-                cube[i][1] -= cube_cnt**3
-                length = d*cube_cnt
-                cnt += cube_cnt**3
+            length = cube[i][0]
+            if length == 1:
+                n = L*W*H
+                if n <= cube[i][1]:
+                    cnt += n
+                    cube[i][1] -= n
+                    break
+            elif length <= L and length <= W and length <= H and cube[i][1] > 0:
+                cube[i][1] -= 1
+                cnt += 1
                 box.append([L-length, W, length])
                 box.append([length, W-length, length])
                 box.append([L, W, H-length])
@@ -26,5 +30,7 @@ l, w, h = map(int, input().split())
 N = int(input())
 cube = list()
 for _ in range(N):
-    cube.append(list(map(int, input().split())))
+    a, b = map(int, input().split())
+    cube.append([2**a, b])
+
 get_cube_cnt(l, w, h, N, cube)
