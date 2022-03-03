cb6fd53bfa36c17abf49271ebc43ee275f2f9716
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-03-04 01:08:09 +0900

    Update BJ12865.py

--- BJ12865.py
+++ BJ12865.py
@@ -1,1 +1,11 @@
 import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+product = []
+for _ in range(N):
+    product.append(list(map(int, input().split())))
+dp = [0]*(K+1)
+for W, V in product:
+    for i in range(K, W-1, -1):
+        dp[i] = max(dp[i], dp[i-W] + V)
+print(dp[-1])
