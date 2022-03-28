1be483808106fa78f0ca87e44ed5b4da12254424
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-03-28 15:34:07 +0900

    Update BJ16234.cpp

--- BJ16234.cpp
+++ BJ16234.cpp
@@ -2,154 +2,108 @@
 //  main.cpp
 //  BJ16234
 //
-//  Created by Hwayeon on 2021/08/05.
+//  Created by 최화연 on 2022/03/28.
 //
 
 #include <iostream>
-#include <cmath>
-using namespace  std;
+#include <queue>
+using namespace std;
+
 int N, L, R;
-int A[50][50] ={0,};
+int A[100][100] = {0, };
+int unity[100][100] = {0, };
 int day = 0;
 
-void move_population(){
-    int unity[50][50] = {0,};
-    int u = 0;
-    int unit[2501][2] = {0,};
-    
-    //1. 국경선 체크
-    for(int y=0; y<N; y++){
-        for(int x=0; x<N; x++){
-            int r_x = x+1;
-            int r_y = y;
-            int b_x = x;
-            int b_y = y+1;
-            if(r_x < N){
-                //오른쪽 체크
-                int r_differ = abs(A[y][x]-A[r_y][r_x]);
-                if(r_differ >= L && r_differ <= R){
-                    //(r_x, r_y) 나라가 연합이 없는 경우,
-                    if(unity[r_y][r_x] == 0){
-                        //(x, y) 나라가 연합이 있는 경우, 연합 추가
-                        if(unity[y][x] != 0){
-                            unity[r_y][r_x] = unity[y][x];
-                            unit[unity[y][x]][0] += A[r_y][r_x];
-                            unit[unity[y][x]][1]++;
-                        }
-                        //(x, y) 나라가 연합이 없는 경우, 연합 생성
-                        else{
-                            u++;
-                            unit[u][0] = A[y][x] + A[r_y][r_x];
-                            unit[u][1] = 2;
-                            unity[y][x] = u;
-                            unity[r_y][r_x] = u;
-                        }
-                    }
-                    //(r_x, r_y) 나라가 연합이 있는 경우,
-                    else if(unity[y][x] != unity[r_y][r_x]){
-                        //(x, y) 나라가 연합이 없는 경우, 연합 추가
-                        if(unity[y][x] == 0){
-                            unity[y][x] = unity[r_y][r_x];
-                            unit[unity[r_y][r_x]][0] += A[y][x];
-                            unit[unity[r_y][r_x]][1] ++;
-                        }
-                        //(x, y) 나라가 연합이 있는 경우, 연합을 합친다
-                        else{
-                            int before_u = unity[r_y][r_x];
-                            unit[unity[y][x]][0] += unit[unity[r_y][r_x]][0];
-                            unit[unity[r_y][r_x]][0] = 0;
-                            unit[unity[y][x]][1] += unit[unity[r_y][r_x]][1];
-                            unit[unity[r_y][r_x]][1] = 0;
-                            int iy = y;
-                            if(b_y < N) iy = b_y;
-                            for(int i=0; i<=iy; i++){
-                                for(int j=0; j<N; j++){
-                                    if(unity[i][j] == before_u){
-                                        unity[i][j] = unity[y][x];
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    
-                }
-            }
-            if(b_y < N){
-                //아래쪽 체크
-                int b_differ = abs(A[y][x]-A[b_y][b_x]);
-                if(b_differ >= L && b_differ <= R){
-                    //(b_x, b_y) 나라가 연합이 없는 경우,
-                    if(unity[b_y][b_x] == 0){
-                        //(x, y) 나라가 연합이 있는 경우, 연합 추가
-                        if(unity[y][x] != 0){
-                            unity[b_y][b_x] = unity[y][x];
-                            unit[unity[y][x]][0] += A[b_y][b_x];
-                            unit[unity[y][x]][1]++;
-                        }
-                        //(x, y) 나라가 연합이 없는 경우, 연합 생성
-                        else{
-                            u++;
-                            unit[u][0] = A[y][x] + A[b_y][b_x];
-                            unit[u][1] = 2;
-                            unity[y][x] = u;
-                            unity[b_y][b_x] = u;
-                        }
-                    }
-                    //(b_x, b_y) 나라가 연합이 있는 경우,
-                    else if(unity[y][x] != unity[b_y][b_x]){
-                        //(x, y) 나라가 연합이 없는 경우, 연합 추가
-                        if(unity[y][x] == 0){
-                            unity[y][x] = unity[b_y][b_x];
-                            unit[unity[b_y][b_x]][0] += A[y][x];
-                            unit[unity[b_y][b_x]][1] ++;
-                        }
-                        //(x, y) 나라가 연합이 있는 경우,
-                        else{
-                            int before_u = unity[b_y][b_x];
-                            unit[unity[y][x]][0] += unit[unity[b_y][b_x]][0];
-                            unit[unity[y][x]][1] += unit[unity[b_y][b_x]][1];
-                            unit[unity[b_y][b_x]][0] = 0;
-                            unit[unity[b_y][b_x]][1] = 0;
-                            for(int i=0; i<=b_y; i++){
-                                for(int j=0; j<N; j++){
-                                    if(unity[i][j] == before_u){
-                                        unity[i][j] = unity[y][x];
-                                    }
-                                }
-                            }
-                        }
-                    }
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int open_border() {
+    int cnt = 0;
+    for(int r=0; r<2*N; r++) {
+        for(int c=0; c<2*N; c++) {
+            unity[r][c] = 0;
+        }
+    }
+    for(int r=0; r<N; r++) {
+        for(int c=0; c<N; c++) {
+            unity[r*2][c*2] = 1;
+            for(int d=0; d<4; d++) {
+                int nr = 2*r+2*dy[d];
+                int nc = 2*c+2*dx[d];
+                if(nr < 0 || nr >= 2*N || nc < 0 || nc >= 2*N) continue;
+                int differ = abs(A[r*2][c*2]-A[nr][nc]);
+                if(L <= differ && differ <= R) {
+                    unity[r*2+dy[d]][c*2+dx[d]] = 1;
+                    cnt ++;
                 }
             }
         }
     }
-    //연합이 없는 경우, 종료
-    if(u == 0) return;
-    //인구 이동
-    day++;
-    for(int i=1; i<=u; i++){
-        if(unit[i][1] > 0){
-            int np = unit[i][0]/unit[i][1];
-            for(int y=0; y<N; y++){
-                for(int x=0; x<N; x++){
-                    if(unity[y][x] == i){
-                        A[y][x] = np;
-                    }
+    return cnt;
+}
+
+void make_unity(int sr, int sc) {
+    int p_num = A[sr][sc];
+    int countries = 1;
+    unity[sr][sc] = 2;
+    queue<pair<int, int>> q;
+    q.push({sr, sc});
+    vector<pair<int, int>> loc;
+    loc.push_back({sr, sc});
+    while(!q.empty()) {
+        int r = q.front().first;
+        int c = q.front().second;
+        q.pop();
+        for(int i=0; i<4; i++) {
+            int nr = r + dy[i];
+            int nc = c + dx[i];
+            if(nr < 0 || nr >= 2*N || nc < 0 || nc >= 2*N) continue;
+            if(unity[nr][nc] == 1) {
+                unity[nr][nc] = 2;
+                if ((nr%2 + nc%2) == 0) {
+                    p_num += A[nr][nc];
+                    countries += 1;
+                    loc.push_back({nr, nc});
                 }
+                q.push({nr, nc});
             }
         }
     }
-    move_population();
+    int new_num = p_num/countries;
+    for(int i=0; i<loc.size(); i++) {
+        A[loc[i].first][loc[i].second] = new_num;
+    }
+}
+
+void get_unities() {
+    for(int r=0; r<N; r++) {
+        for(int c=0; c<N; c++) {
+            if(unity[2*r][2*c] == 1) {
+                make_unity(2*r, 2*c);
+            }
+        }
+    }
+}
+
+void move_people() {
+    while(true) {
+        int border = open_border();
+        if (border == 0) break;
+        get_unities();
+        day ++;
+    }
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N >> L >> R;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cin >> A[i][j];
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            cin >> A[i*2][j*2];
         }
     }
-    move_population();
+    
+    move_people();
     cout << day << endl;
+    
     return 0;
 }
