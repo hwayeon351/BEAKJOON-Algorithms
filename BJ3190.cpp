07f8bc16a58b85184bac286d6935c913d4557915
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-08 10:45:31 +0900

    Update BJ3190.cpp

--- BJ3190.cpp
+++ BJ3190.cpp
@@ -2,78 +2,90 @@
 //  main.cpp
 //  BJ3190
 //
-//  Created by Hwayeon on 2021/07/18.
+//  Created by 최화연 on 2022/04/08.
 //
 
 #include <iostream>
+#include <vector>
+#include <queue>
 #include <deque>
 using namespace std;
 
-int map[101][101] = {0,};
 int N, K, L;
-int change[100][2] = {0, };
+int map[101][101] = {0, };
+queue<pair<int, char>> cmds;
 
-//+ -> 오른쪽 회전 D => (i+1)%4
-//- -> 왼쪽 회전 L => (i+3)%4
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int timer = 0;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+int sec = 0;
 
-deque<pair<int, int>> snake;
-int s_dir = 0;
+struct Snake {
+    deque<pair<int, int>> body = {{1, 1}};
+    int dir = 2;
+};
+Snake snake;
 
-void start_dummy(){
-    map[1][1] = 1;
-    snake.push_back(make_pair(1, 1));
-    int l = 0;
-    while(1){
-        if(l<=L-1 and change[l][0] == timer){
-            s_dir = (s_dir + change[l][1])%4;
-            l++;
+void play_game() {
+    map[1][1] = 2;
+    
+    while (true) {
+        sec++;
+        
+        //1. 뱀의 몸길이를 늘려 머리를 다음칸에 위치시킨다
+        int head_x = snake.body.front().first+dx[snake.dir];
+        int head_y = snake.body.front().second+dy[snake.dir];
+        
+        //2. 벽에 부딪힌 경우, 게임 끝
+        if (head_x <= 0 || head_x > N || head_y <= 0 || head_y > N) return;
+        //3. 몸에 부딪힌 경우, 게임 끝
+        if (map[head_y][head_x] == 2) return;
+        
+        snake.body.push_front({head_x, head_y});
+        
+        //4. 이동한 칸에 사과가 있는 경우,
+        if (map[head_y][head_x] == 1) {
+            map[head_y][head_x] = 2;
         }
-        timer ++;
-        //머리 이동할 다음 칸
-        int nx = snake.front().first + dx[s_dir];
-        int ny = snake.front().second + dy[s_dir];
-        //벽에 부딪힌 경우
-        if(nx < 1 || nx > N || ny < 1 || ny > N) return;
-        //자기 몸과 부딪힌 경우
-        if(map[ny][nx] == 1) return;
-        //사과가 있는 경우
-        else if(map[ny][nx] == 4){
-            //머리만 이동
-            map[ny][nx] = 1;
-            snake.push_front(make_pair(nx, ny));
+        
+        //5. 이동한 칸에 사과가 없는 경우,
+        else if (map[head_y][head_x] == 0) {
+            map[head_y][head_x] = 2;
+            int tail_x = snake.body.back().first;
+            int tail_y = snake.body.back().second;
+            snake.body.pop_back();
+            map[tail_y][tail_x] = 0;
         }
-        //사과가 없는 경우
-        else{
-            //머리 이동
-            map[ny][nx] = 1;
-            snake.push_front(make_pair(nx, ny));
-            //꼬리 이동
-            pair<int, int> tail = snake.back();
-            map[tail.second][tail.first] = 0;
-            snake.pop_back();
+        
+        if (cmds.front().first == sec) {
+            if (cmds.front().second == 'L') {
+                snake.dir = (snake.dir - 1 + 4) % 4;
+            } else {
+                snake.dir = (snake.dir + 1) % 4;
+            }
+            cmds.pop();
         }
+        
     }
 }
 
 int main(int argc, const char * argv[]) {
-    cin >> N >> K;
-    for(int i=0; i<K; i++){
-        int x, y;
+    cin >> N;
+    cin >> K;
+    for(int i=0; i<K; i++) {
+        int y, x;
         cin >> y >> x;
-        map[y][x] = 4;
+        map[y][x] = 1;
     }
     cin >> L;
-    for(int i=0; i<L; i++){
-        char dir;
-        cin >> change[i][0] >> dir;
-        if(dir == 'L') change[i][1] = 3;
-        else change[i][1] = 1;
+    for(int i=0; i<L; i++) {
+        int X;
+        char C;
+        cin >> X >> C;
+        cmds.push({X, C});
     }
     
-    start_dummy();
-    cout << timer << endl;
+    play_game();
+    cout << sec << endl;
+    
     return 0;
 }
