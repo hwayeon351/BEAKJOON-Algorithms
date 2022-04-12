bdffec93fd2173a7bdf0ce59a3a0976d5b5ae6f5
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-12 16:20:08 +0900

    Update BJ17142.cpp

--- BJ17142.cpp
+++ BJ17142.cpp
@@ -2,120 +2,99 @@
 //  main.cpp
 //  BJ17142
 //
-//  Created by Hwayeon on 2021/08/18.
+//  Created by 최화연 on 2022/04/12.
 //
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 using namespace std;
 
-int N, M;
 int lab[50][50] = {0, };
 int copy_lab[50][50] = {0, };
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int N, M;
+int hole_cnt = 0;
+
 vector<pair<int, int>> virus;
-vector<pair<int, int>> v;
-vector<pair<int, int>> nv;
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+vector<pair<int, int>> active_virus;
 int min_sec = -1;
-int max_virus = 0;
-int v_cnt = 0;
 
-void copy_laboratory(){
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            copy_lab[i][j] = lab[i][j];
-        }
+void spread_virus() {
+    memcpy(copy_lab, lab, sizeof(copy_lab));
+    queue<pair<pair<int, int>, int>> q;
+    for(int i=0; i<M; i++) {
+        q.push({{active_virus[i].first, active_virus[i].second}, 0});
+        copy_lab[active_virus[i].second][active_virus[i].first] = 3;
     }
-}
-
-void spread_virus(){
-    int sec = 0;
-    copy_laboratory();
-    queue<vector<pair<int, int>>> q;
-    q.push(virus);
-    //활성 상태로 바꾸기
-    for(int i=0; i<q.front().size(); i++){
-        copy_lab[q.front()[i].second][q.front()[i].first] = 3;
-    }
-    int cnt = v_cnt;
-
-    int x, y, nx, ny;
-    bool ck = false;
-    while(!q.empty()){
-        v.clear();
-        nv.clear();
-        if(cnt == max_virus){
-            ck = true;
-            break;
-        }
-        v = q.front();
+    int hole = 0;
+    
+    while(!q.empty()) {
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int sec = q.front().second;
         q.pop();
-        for(int i=0; i<v.size(); i++){
-            x = v[i].first;
-            y = v[i].second;
-            for(int d=0; d<4; d++){
-                nx = x+dx[d];
-                ny = y+dy[d];
-                if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-                //비활성 상태의 바이러스인 경우
-                if(copy_lab[ny][nx] == 2){
-                    copy_lab[ny][nx] = 3;
-                    nv.push_back({nx, ny});
-                }
-                //빈 칸인 경우
-                else if(copy_lab[ny][nx] == 0){
-                    copy_lab[ny][nx] = 3;
-                    nv.push_back({nx, ny});
-                    cnt ++;
+        
+        for(int d=0; d<4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            //비활성화 바이러스인 경우,
+            if (copy_lab[ny][nx] == 2) {
+                copy_lab[ny][nx] = 3;
+                q.push({{nx, ny}, sec+1});
+            }
+            //빈 칸인 경우,
+            else if (copy_lab[ny][nx] == 0) {
+                copy_lab[ny][nx] = 3;
+                q.push({{nx, ny}, sec+1});
+                hole ++;
+                if (hole == hole_cnt) {
+                    if (min_sec == -1 || min_sec > sec+1) {
+                        min_sec = sec+1;
+                    }
+                    return;
                 }
             }
         }
-        if(nv.size() > 0) q.push(nv);
-        sec++;
-    }
-    if(ck){
-        if(min_sec == -1) min_sec = sec;
-        else if(sec < min_sec) min_sec = sec;
     }
 }
 
-void select_virus(int x, int y){
-    if(virus.size() == M){
+void pick_M_virus(int i, int cnt) {
+    if (cnt == M) {
         spread_virus();
         return;
     }
-    for(int i=y; i<N; i++){
-        if(i > y) x = 0;
-        for(int j=x; j<N; j++){
-            if(lab[i][j] == 2){
-                virus.push_back({j, i});
-                select_virus(j+1, i);
-                virus.pop_back();
-            }
-        }
+    
+    for (int j=i; j<virus.size(); j++) {
+        active_virus.push_back({virus[j].first, virus[j].second});
+        pick_M_virus(j+1, cnt+1);
+        active_virus.pop_back();
     }
+    
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N >> M;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
             cin >> lab[i][j];
-            if(lab[i][j] == 2){
-                v_cnt++;
-                max_virus++;
+            if (lab[i][j] == 0) {
+                hole_cnt ++;
             }
-            else if(lab[i][j] == 0) max_virus++;
+            else if (lab[i][j] == 2) {
+                virus.push_back({j, i});
+            }
         }
     }
-    if(v_cnt == max_virus){
-        cout << 0 << endl;
-    }
-    else{
-        select_virus(0, 0);
+    if (hole_cnt == 0) cout << 0 << endl;
+    else {
+        pick_M_virus(0, 0);
         cout << min_sec << endl;
     }
+
     return 0;
 }
