8096ae4c0d882773116a6e1c681b7e4f7460f95e
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-16 20:54:37 +0900

    Update BJ17779.cpp

--- BJ17779.cpp
+++ BJ17779.cpp
@@ -2,164 +2,134 @@
 //  main.cpp
 //  BJ17779
 //
-//  Created by Hwayeon on 2021/08/19.
+//  Created by 최화연 on 2022/04/16.
 //
 
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <queue>
 using namespace std;
 
 int N;
 int A[21][21] = {0, };
-int area[21][21] = {0, };
-int population[6] = {0, };
-int max_A = 0;
-int min_A = 400001;
-int min_differ = 400001;
-int d1, d2 = 0;
-int px, py = 0;
+int vst[21][21] = {0, };
+int min_differ = -1;
+int total_A = 0;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
-
-void make_district1(){
-    for(int x=1; x<px+d1; x++){
-        for(int y=1; y<=py; y++){
-            if(area[x][y] == 0){
-                area[x][y] = 1;
-                population[1] += A[x][y];
+int get_area_5(int sx, int sy) {
+    if (vst[sx][sy]) return 0;
+    int sum = A[sx][sy];
+    vst[sx][sy] = 1;
+    queue<pair<int, int>> q;
+    q.push({sx, sy});
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int d=0; d<4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (1 <= nx && nx <= N && 1 <= ny && ny <= N) {
+                if (vst[nx][ny]) continue;
+                vst[nx][ny] = 1;
+                sum += A[nx][ny];
+                q.push({nx, ny});
             }
         }
     }
-    if(population[1] > max_A) max_A = population[1];
-    if(population[1] < min_A) min_A = population[1];
+    
+    return sum;
 }
 
-void make_district2(){
-    for(int x=1; x<=px+d2; x++){
-        for(int y=py+1; y<=N; y++){
-            if(area[x][y] == 0){
-                area[x][y] = 2;
-                population[2] += A[x][y];
-            }
-        }
-    }
-    if(population[2] > max_A) max_A = population[2];
-    if(population[2] < min_A) min_A = population[2];
-}
-
-void make_district3(){
-    for(int x=px+d1; x<=N; x++){
-        for(int y=1; y<py-d1+d2; y++){
-            if(area[x][y] == 0){
-                area[x][y] = 3;
-                population[3] += A[x][y];
-            }
-        }
-    }
-    if(population[3] > max_A) max_A = population[3];
-    if(population[3] < min_A) min_A = population[3];
-}
-
-void make_district4(){
-    for(int x=px+d2+1; x<=N; x++){
-        for(int y=py-d1+d2; y<=N; y++){
-            if(area[x][y] == 0){
-                area[x][y] = 4;
-                population[4] += A[x][y];
-            }
-        }
-    }
-    if(population[4] > max_A) max_A = population[4];
-    if(population[4] < min_A) min_A = population[4];
-}
-
-//dfs
-void make_district5(int x, int y){
-    if(area[x][y]==0){
-        area[x][y] = 5;
-        population[5] += A[x][y];
-    }
-    for(int i=0; i<4; i++){
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if(area[nx][ny] == 0) make_district5(nx, ny);
-    }
-}
-
-void make_border(){
-    memset(area, 0, sizeof(area));
-    memset(population, 0, sizeof(population));
-    //1.
-    int nx, ny;
-    for(int d=0; d<=d1; d++){
-        nx = px + d;
-        ny = py - d;
-        area[nx][ny] = 5;
-        population[5] += A[nx][ny];
-    }
-    //2.
-    for(int d=0; d<=d2; d++){
-        nx = px + d;
-        ny = py + d;
-        if(area[nx][ny] == 0){
-            area[nx][ny] = 5;
-            population[5] += A[nx][ny];
-        }
-    }
-    //3.
-    for(int d=0; d<=d2; d++){
-        nx = px + d1 + d;
-        ny = py - d1 + d;
-        if(area[nx][ny] == 0){
-            area[nx][ny] = 5;
-            population[5] += A[nx][ny];
-        }
-    }
-    //4.
-    for(int d=0; d<=d1; d++){
-        nx = px + d2 + d;
-        ny = py + d2 - d;
-        if(area[nx][ny] == 0){
-            area[nx][ny] = 5;
-            population[5] += A[nx][ny];
-        }
-    }
-    //경계선 안 5 채워넣기
-    for(int d=0; d<d1; d++){
-        nx = px + d;
-        ny = py - d;
-        make_district5(nx+1, ny);
-    }
-    for(int d=1; d<=d1; d++){
-        nx = px + d2 + d;
-        ny = py + d2 - d;
-        make_district5(nx-1, ny);
-    }
-    if(population[5] > max_A) max_A = population[5];
-    if(population[5] < min_A) min_A = population[5];
-}
-
-void select_pivot(){
-    for(int dd1=1; dd1<=N-1; dd1++){
-        for(int dd2=1; dd2<=N-1; dd2++){
-            for(int x=1; x<=N-2; x++){
-                for(int y=2; y<=N-1; y++){
-                    //기준점과 경계의 길이가 될 수 있는 조건
-                    if(x+dd1+dd2 <= N && y-dd1 >= 1 && y+dd2 <= N){
-                        max_A = 0;
-                        min_A = 400001;
-                        px = x;
-                        py = y;
-                        d1 = dd1;
-                        d2 = dd2;
-                        make_border();
-                        make_district1();
-                        make_district2();
-                        make_district3();
-                        make_district4();
-                        if(min_differ > (max_A-min_A)) min_differ = max_A-min_A;
+void divide_group() {
+    for(int d1=1; d1<N; d1++) {
+        for (int d2=1; d2<N; d2++) {
+            for (int y=1; y<=N; y++) {
+                for (int x=1; x<=N; x++) {
+                    if (!(1 <= x && x < x+d1+d2 && x+d1+d2 <= N)) continue;
+                    if (!(1 <= y-d1 && y-d1 < y && y < y+d2 && y+d2 <= N)) continue;
+                    
+                    memset(vst, 0, sizeof(vst));
+                    int a[6] = {0, };
+                    
+                    //경계선
+                    for (int i=0; i<=d1; i++) {
+                        if (!vst[x+i][y-i]) {
+                            vst[x+i][y-i] = 1;
+                            a[5] += A[x+i][y-i];
+                        }
+                        if (!vst[x+d2+i][y+d2-i]) {
+                            vst[x+d2+i][y+d2-i] = 1;
+                            a[5] += A[x+d2+i][y+d2-i];
+                        }
                     }
+                    for (int i=0; i<=d2; i++) {
+                        if (!vst[x+d1+i][y-d1+i]) {
+                            vst[x+d1+i][y-d1+i] = 1;
+                            a[5] += A[x+d1+i][y-d1+i];
+                        }
+                        if (!vst[x+i][y+i]) {
+                            vst[x+i][y+i] = 1;
+                            a[5] += A[x+i][y+i];
+                        }
+                    }
+                    
+                    //경계선 내부
+                    for (int i=0; i<d1; i++) {
+                        a[5] += get_area_5(x+i+1, y-i);
+                    }
+                    for (int i=1; i<=d1; i++) {
+                        a[5] += get_area_5(x+d2+d1-1, y+d2-d1);
+                    }
+                    
+                    //1번
+                    for (int r=1; r<x+d1; r++) {
+                        for (int c=1; c<=y; c++) {
+                            if (vst[r][c]) continue;
+                            vst[r][c] = 1;
+                            a[1] += A[r][c];
+                        }
+                    }
+                    
+                    //2번
+                    for (int r=1; r<=x+d2; r++) {
+                        for (int c=y+1; c<=N; c++) {
+                            if (vst[r][c]) continue;
+                            vst[r][c] = 1;
+                            a[2] += A[r][c];
+                        }
+                    }
+                    
+                    //3번
+                    for (int r=x+d1; r<=N; r++) {
+                        for (int c=1; c<y-d1+d2; c++) {
+                            if (vst[r][c]) continue;
+                            vst[r][c] = 1;
+                            a[3] += A[r][c];
+                        }
+                    }
+                    
+                    //4번
+                    for (int r=x+d2; r<=N; r++) {
+                        for (int c=y-d1+d2; c<=N; c++) {
+                            if (vst[r][c]) continue;
+                            vst[r][c] = 1;
+                            a[4] += A[r][c];
+                        }
+                    }
+                    
+                    int max_a = 0;
+                    int min_a = total_A;
+                    for (int i=1; i<=5; i++) {
+                        if (a[i] > max_a) max_a = a[i];
+                        if (a[i] < min_a) min_a = a[i];
+                    }
+
+                    int differ = max_a - min_a;
+
+                    if (min_differ == -1 || min_differ > differ) min_differ = differ;
                 }
             }
         }
@@ -168,12 +138,15 @@ void select_pivot(){
 
 int main(int argc, const char * argv[]) {
     cin >> N;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            cin >> A[i][j];
+    for (int r=1; r<=N; r++) {
+        for (int c=1; c<=N; c++) {
+            cin >> A[r][c];
+            total_A += A[r][c];
         }
     }
-    select_pivot();
+    divide_group();
+    
     cout << min_differ << endl;
+    
     return 0;
 }
