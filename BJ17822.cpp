87c4d414322d87feeeea9df94516a30336059eae
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-25 16:03:59 +0900

    Update BJ17822.cpp

--- BJ17822.cpp
+++ BJ17822.cpp
@@ -2,142 +2,178 @@
 //  main.cpp
 //  BJ17822
 //
-//  Created by Hwayeon on 2021/09/21.
+//  Created by 최화연 on 2022/04/25.
 //
 
 #include <iostream>
 #include <deque>
+#include <cstring>
+#include <queue>
 using namespace std;
 
 int N, M, T;
-int board[50][50] = {0, };
 
-struct Command{
-    int x;
-    int d;
-    int k;
-};
-deque<Command> cmds;
-Command cmd;
-int answer = 0;
+//circle[i][j] = i번째 원판에 적힌 j번째 수
+deque<deque<int>> circles = {{}, };
+int visits[51][51] = {0, };
 
-void rotate(){
-    deque<int> x_board;
-    deque<pair<int, int>> erase;
+void rotate_circle(int x, int d, int k) {
+    for (int i=x; i<=N; i+=x) {
+        //시계
+        if (d == 0) {
+            for (int j=0; j<k; j++) {
+                int num = circles[i].back();
+                circles[i].pop_back();
+                circles[i].push_front(num);
+            }
+        }
+        //반시계
+        else {
+            for (int j=0; j<k; j++) {
+                int num = circles[i].front();
+                circles[i].pop_front();
+                circles[i].push_back(num);
+            }
+        }
+    }
+}
+
+int erase_num(int si, int sj, int num) {
+    int cnt = 1;
+    visits[si][sj] = 1;
+    queue<pair<int, int>> q;
+    q.push({si, sj});
     
-    while(!cmds.empty()){
-        cmd = cmds.front();
-        cmds.pop_front();
-        for(int x=cmd.x-1; x<N; x+=cmd.x){
-            x_board.clear();
-            for(int j=0; j<M; j++){
-                x_board.push_back(board[x][j]);
+    while (!q.empty()) {
+        int i = q.front().first;
+        int j = q.front().second;
+        q.pop();
+        if (j == M-1) {
+            if (circles[i][0] == num && visits[i][0] == 0) {
+                visits[i][0] = 1;
+                circles[i][0] = 0;
+                cnt ++;
+                q.push({i, 0});
             }
-            if(cmd.d == 0){
-                for(int k=0; k<cmd.k; k++){
-                    int temp = x_board.back();
-                    x_board.pop_back();
-                    x_board.push_front(temp);
-                }
+            if (circles[i][M-2] == num && visits[i][M-2] == 0) {
+                visits[i][M-2] = 1;
+                circles[i][M-2] = 0;
+                cnt ++;
+                q.push({i, M-2});
             }
-            else{
-                for(int k=0; k<cmd.k; k++){
-                    int temp = x_board.front();
-                    x_board.pop_front();
-                    x_board.push_back(temp);
-                }
+        }
+        else if (j == 0) {
+            if (circles[i][1] == num && visits[i][1] == 0) {
+                visits[i][1] = 1;
+                circles[i][1] = 0;
+                cnt ++;
+                q.push({i, 1});
             }
-            for(int j=0; j<M; j++){
-                board[x][j] = x_board[j];
+            if (circles[i][M-1] == num && visits[i][M-1] == 0) {
+                visits[i][M-1] = 1;
+                circles[i][M-1] = 0;
+                cnt ++;
+                q.push({i, M-1});
+            }
+        }
+        else {
+            if (circles[i][j-1] == num && visits[i][j-1] == 0) {
+                visits[i][j-1] = 1;
+                circles[i][j-1] = 0;
+                cnt ++;
+                q.push({i, j-1});
+            }
+            if (circles[i][j+1] == num && visits[i][j+1] == 0) {
+                visits[i][j+1] = 1;
+                circles[i][j+1] = 0;
+                cnt ++;
+                q.push({i, j+1});
             }
         }
         
-        erase.clear();
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(board[i][j] == 0) continue;
-                if(j < M-1){
-                    if(board[i][j] == board[i][j+1]){
-                        erase.push_back({j, i});
-                        erase.push_back({j+1, i});
-                        if(i < N-1){
-                            if(board[i][j] == board[i+1][j]){
-                                erase.push_back({j, i+1});
-                            }
-                        }
-                    }
-                    else if(i < N-1){
-                        if(board[i][j] == board[i+1][j]){
-                            erase.push_back({j, i});
-                            erase.push_back({j, i+1});
-                        }
-                    }
-                }
-                else {
-                    if(i < N-1){
-                        if(board[i][j] == board[i+1][j]){
-                            erase.push_back({j, i});
-                            erase.push_back({j, i+1});
-                        }
-                    }
-                    if(board[i][j] == board[i][0]){
-                        erase.push_back({j, i});
-                        erase.push_back({0, i});
-                    }
-                }
+        if (i < N) {
+            if (circles[i+1][j] == num && visits[i+1][j] == 0) {
+                visits[i+1][j] = 1;
+                circles[i+1][j] = 0;
+                cnt ++;
+                q.push({i+1, j});
             }
         }
-        if(erase.size() > 0){
-            for(int i=0; i<erase.size(); i++){
-                board[erase[i].second][erase[i].first] = 0;
+        
+        if (i > 1) {
+            if (circles[i-1][j] == num && visits[i-1][j] == 0) {
+                visits[i-1][j] = 1;
+                circles[i-1][j] = 0;
+                cnt ++;
+                q.push({i-1, j});
             }
         }
-        else{
-            int cnt = 0;
-            int sum = 0;
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    if(board[i][j] == 0) continue;
-                    sum += board[i][j];
-                    cnt++;
-                }
-            }
-            if(sum == 0){
-                answer = 0;
-                break;
-            }
-            double avg = (double)sum/cnt;
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    if(board[i][j] == 0) continue;
-                    if(board[i][j] < avg) board[i][j]++;
-                    else if(board[i][j] > avg) board[i][j] --;
-                }
-            }
-        }
-        answer = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(board[i][j] == 0) continue;
-                answer += board[i][j];
-            }
-        }
-        if(answer == 0) break;
     }
+    
+    if (cnt > 1) circles[si][sj] = 0;
+    return cnt;
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N >> M >> T;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            cin >> board[i][j];
+    for (int i=1; i<=N; i++) {
+        deque<int> circle;
+        for (int j=1; j<=M; j++) {
+            int n;
+            cin >> n;
+            circle.push_back(n);
+        }
+        circles.push_back(circle);
+    }
+    
+    int x, d, k;
+    for (int t=0; t<T; t++) {
+        cin >> x >> d >> k;
+        rotate_circle(x, d, k);
+        
+        memset(visits, 0, sizeof(visits));
+        int erase_cnt = 0;
+        int non_cnt = 0;
+        int sum = 0;
+        for (int i=1; i<=N; i++) {
+            for (int j=0; j<M; j++) {
+                if (visits[i][j] || circles[i][j] == 0) continue;
+                int num = circles[i][j];
+                int same = erase_num(i, j, num);
+                if (same == 1) {
+                    sum += num;
+                    non_cnt ++;
+                }
+                else {
+                    sum += same*num;
+                    erase_cnt += same;
+                }
+            }
+        }
+        
+        if(sum == 0) {
+            cout << 0 << endl;
+            return 0;
+        }
+        
+        if(erase_cnt == 0) {
+            double average = (double)sum/double(non_cnt);
+            for (int i=1; i<=N; i++) {
+                for (int j=0; j<M; j++) {
+                    if (circles[i][j] == 0) continue;
+                    if (circles[i][j] > average) circles[i][j] --;
+                    else if (circles[i][j] < average) circles[i][j] ++;
+                }
+            }
         }
     }
-    for(int t=0; t<T; t++){
-        cin >> cmd.x >> cmd.d >> cmd.k;
-        cmds.push_back(cmd);
+    
+    int answer = 0;
+    for (int i=1; i<=N; i++) {
+        for (int j=0; j<M; j++) {
+            answer += circles[i][j];
+        }
     }
-    rotate();
     cout << answer << endl;
     
     return 0;
