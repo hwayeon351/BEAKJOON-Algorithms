56f0e13a2c685d27f5fc60cd44201570ce5afc55
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-26 00:36:58 +0900

    Update BJ21608.cpp

--- BJ21608.cpp
+++ BJ21608.cpp
@@ -1,123 +1,124 @@
 //
 //  main.cpp
-//  BJ21608_1
+//  BJ21608
 //
-//  Created by Hwayeon on 2021/10/17.
+//  Created by 최화연 on 2022/04/26.
 //
 
 #include <iostream>
-#include <math.h>
-#include <vector>
+#include <cmath>
 #include <queue>
 using namespace std;
 
 int N;
-int room[21][21] = {0, };
 
-struct Student{
-    int s_id;
+struct Student {
+    int s_num = 0;
     int like[4] = {0, };
 };
-Student st;
-Student students[401];
-vector<Student> cmds;
+Student student;
+Student room[21][21];
+queue<Student> students;
 
-struct Seat{
-    int x;
-    int y;
-    int hole = 0;
+struct Seat {
+    int r;
+    int c;
     int like = 0;
+    int hole = 0;
 };
-Seat seat;
-struct cmp{
-    bool operator()(Seat s1, Seat s2){
-        if(s1.like == s2.like){
-            if(s1.hole == s2.hole){
-                if(s1.y == s2.y){
-                    return s1.x > s2.x;
+struct cmp {
+    bool operator() (Seat s1, Seat s2) {
+        if (s1.like == s2.like) {
+            if (s1.hole == s2.hole) {
+                if (s1.r == s2.r) {
+                    return s1.c > s2.c;
                 }
-                return s1.y > s2.y;
+                return s1.r > s2.r;
             }
             return s1.hole < s2.hole;
         }
         return s1.like < s2.like;
     }
 };
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, -1, 0, 1};
+Seat seat;
 
-int satisfy = 0;
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, -1, 0, 1};
 
-void find_seat(Student st){
-    priority_queue<Seat, vector<Seat>, cmp> pq;
-    for(int y=1; y<=N; y++){
-        for(int x=1; x<=N; x++){
-            if(room[y][x] > 0) continue;
-            Seat new_seat;
-            new_seat.x = x;
-            new_seat.y = y;
-            //1. 인접한 좋아하는 학생 수와 빈자리 수 구하기
-            for(int d=0; d<4; d++){
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if(nx<=0 || nx>N || ny<=0 || ny>N) continue;
-                if(room[ny][nx] == 0) new_seat.hole ++;
-                else{
-                    for(int i=0; i<4; i++){
-                        if(room[ny][nx] == st.like[i]){
-                            new_seat.like++;
+void assign_seat() {
+    while (!students.empty()) {
+        student = students.front();
+        students.pop();
+        
+        priority_queue<Seat, vector<Seat>, cmp> pq;
+        for (int r=1; r<=N; r++) {
+            for (int c=1; c<=N; c++) {
+                if (room[r][c].s_num > 0) continue;
+                Seat s;
+                s.r = r;
+                s.c = c;
+                for (int d=0; d<4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    if (nr < 1 || nr > N || nc < 1 || nc > N) continue;
+                    if (room[nr][nc].s_num == 0) {
+                        s.hole ++;
+                        continue;
+                    }
+                    for (int i=0; i<4; i++) {
+                        if (room[nr][nc].s_num == student.like[i]) {
+                            s.like ++;
                             break;
                         }
                     }
                 }
+                pq.push(s);
             }
-            //2. 자리 후보 우선순위 큐에 추가
-            pq.push(new_seat);
         }
+        
+        seat = pq.top();
+        pq.pop();
+        room[seat.r][seat.c] = student;
     }
-    //자리 선정
-    seat = pq.top();
-    room[seat.y][seat.x] = st.s_id;
 }
 
-void get_satisfy(){
-    for(int y=1; y<=N; y++){
-        for(int x=1; x<=N; x++){
-            int s_id = room[y][x];
-            int cnt = 0;
-            //인접한 좋아하는 친구 수 카운트
-            for(int d=0; d<4; d++){
-                int nx = x+dx[d];
-                int ny = y+dy[d];
-                if(nx<=0 || nx>N || ny<=0 || ny>N) continue;
-                for(int i=0; i<4; i++){
-                    if(room[ny][nx] == students[s_id].like[i]){
-                        cnt++;
+int get_satisfy() {
+    int answer = 0;
+    for (int r=1; r<=N; r++) {
+        for (int c=1; c<=N; c++) {
+            int like = 0;
+            for (int d=0; d<4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+                if (nr < 1 || nr > N || nc < 1 || nc > N) continue;
+                for (int i=0; i<4; i++) {
+                    if (room[nr][nc].s_num == room[r][c].like[i]) {
+                        like ++;
                         break;
                     }
                 }
             }
-            if(cnt == 0) continue;
-            satisfy += pow(10, cnt-1);
+            if (like == 0) continue;
+            answer += pow(10, like-1);
         }
     }
+    
+    return answer;
 }
+
 
 int main(int argc, const char * argv[]) {
     cin >> N;
-    for(int i=1; i<=N*N; i++){
-        cin >> st.s_id >> st.like[0] >> st.like[1] >> st.like[2] >> st.like[3];
-        students[st.s_id] = st;
-        cmds.push_back(st);
+    for (int i=0; i<N*N; i++) {
+        cin >> student.s_num;
+        for (int j=0; j<4; j++) {
+            cin >> student.like[j];
+        }
+        students.push(student);
     }
-    //모든 학생을 자리에 앉힌다
-    for(int i=0; i<cmds.size(); i++){
-        st = cmds[i];
-        find_seat(st);
-    }
-    //만족도 구하기
-    get_satisfy();
-    cout << satisfy << endl;
+    
+    assign_seat();
+    cout << get_satisfy() << endl;
     
     return 0;
 }
