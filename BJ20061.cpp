24e5aa6eae718cfe133b430b3e7474eca4a38100
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-28 00:09:00 +0900

    Update BJ20061.cpp

--- BJ20061.cpp
+++ BJ20061.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  BJ20061
 //
-//  Created by Hwayeon on 2021/10/13.
+//  Created by 최화연 on 2022/04/27.
 //
 
 #include <iostream>
@@ -10,276 +10,224 @@
 using namespace std;
 
 int N;
-
-/*
- red board -> 0<=x<=3, 0<=y<=3
- blue board -> 6<=x<=9, 0<=y<=3
- sky blue board -> 4<=x<=5, 0<=y<=3
- green board -> 0<=x<=3, 6<=y<=9
- light green board -> 0<=x<=3, 4<=y<=5
- */
 int board[10][10] = {0, };
-struct Block{
-    int t;
-    int x;
-    int y;
-};
-Block b;
-vector<Block> blocks;
+vector<pair<int, pair<int, int>>> blocks;
+
 int score = 0;
-int blue_blocks = 0;
-int green_blocks = 0;
 
-void put_block1(int bx, int by){
-    //put block on blue board
-    for(int x=4; x<=8; x++){
-        if(board[by][x+1]){
-            board[by][x] = 1;
+void put_block(int t, int r, int c) {
+    //1. 파란색 보드에 블록 놓기
+    switch (t) {
+        case 1:
+            for (int c=3; c<=8; c++) {
+                if (c+1 == 9 && board[r][c+1] == 0) {
+                    board[r][9] = 1;
+                    break;
+                }
+                if (board[r][c+1] == 0) continue;
+                board[r][c] = 1;
+                break;
+            }
             break;
-        }
-        else if(x == 8){
-            board[by][9] = 1;
-        }
-    }
-    //put block on green board
-    for(int y=4; y<=8; y++){
-        if(board[y+1][bx]){
-            board[y][bx] = 1;
+            
+        case 2:
+            for (int c=3; c<=8; c++) {
+                if (c+1 == 9 && board[r][c+1] == 0) {
+                    board[r][9] = 1;
+                    board[r][8] = 1;
+                    break;
+                }
+                if (board[r][c+1] == 0) continue;
+                board[r][c] = 1;
+                board[r][c-1] = 1;
+                break;
+            }
             break;
-        }
-        else if(y == 8){
-            board[9][bx] = 1;
-        }
-    }
-}
-
-void put_block2(int bx, int by){
-    //put block on blue board
-    for(int x=4; x<=8; x++){
-        if(board[by][x+1]){
-            board[by][x] = 1;
-            board[by][x-1] = 1;
+            
+        case 3:
+            for (int c=3; c<=8; c++) {
+                if (c+1 == 9 && board[r][c+1] == 0 && board[r+1][c+1] == 0) {
+                    board[r][9] = 1;
+                    board[r+1][9] = 1;
+                    break;
+                }
+                if (board[r][c+1] == 0 && board[r+1][c+1] == 0) continue;
+                board[r][c] = 1;
+                board[r+1][c] = 1;
+                break;
+            }
             break;
-        }
-        else if(x == 8){
-            board[by][9] = 1;
-            board[by][8] = 1;
-        }
     }
     
-    //put block on green board
-    for(int y=4; y<=8; y++){
-        if(board[y+1][bx] || board[y+1][bx+1]){
-            board[y][bx] = 1;
-            board[y][bx+1] = 1;
-            break;
-        }
-        else if(y == 8){
-            board[9][bx] = 1;
-            board[9][bx+1] = 1;
-        }
-    }
-}
-
-void put_block3(int bx, int by){
-    //put block on blue board
-    for(int x=4; x<=8; x++){
-        if(board[by][x+1] || board[by+1][x+1]){
-            board[by][x] = 1;
-            board[by+1][x] = 1;
-            break;
-        }
-        else if(x == 8){
-            board[by][9] = 1;
-            board[by+1][9] = 1;
-        }
-    }
     
-    //put block on green board
-    for(int y=4; y<=8; y++){
-        if(board[y+1][bx]){
-            board[y][bx] = 1;
-            board[y-1][bx] = 1;
+    //2. 초록색 보드에 블록 놓기
+    switch (t) {
+        case 1:
+            for (int r=3; r<=8; r++) {
+                if (r+1 == 9 && board[r+1][c] == 0) {
+                    board[9][c] = 1;
+                    break;
+                }
+                if (board[r+1][c] == 0) continue;
+                board[r][c] = 1;
+                break;
+            }
             break;
-        }
-        else if(y == 8){
-            board[8][bx] = 1;
-            board[9][bx] = 1;
-        }
+            
+        case 2:
+            for (int r=3; r<=8; r++) {
+                if (r+1 == 9 && board[r+1][c] == 0 && board[r+1][c+1] == 0) {
+                    board[9][c] = 1;
+                    board[9][c+1] = 1;
+                    break;
+                }
+                if (board[r+1][c] == 0 && board[r+1][c+1] == 0) continue;
+                board[r][c] = 1;
+                board[r][c+1] = 1;
+                break;
+            }
+            break;
+            
+        case 3:
+            for (int r=3; r<=8; r++) {
+                if (r+1 == 9 && board[r+1][c] == 0) {
+                    board[9][c] = 1;
+                    board[8][c] = 1;
+                    break;
+                }
+                if (board[r+1][c] == 0) continue;
+                board[r][c] = 1;
+                board[r-1][c] = 1;
+                break;
+            }
+            break;
     }
 }
 
-void check_blue_board(){
-    int x = 9;
-    bool check = true;
-    while(x >= 6){
-        check = true;
-        for(int y=0; y<4; y++){
-            if(!board[y][x]) check = false;
+void check_green_board() {
+    int r=9;
+    while (r > 5) {
+        bool check = true;
+        for (int c=0; c<=3; c++) {
+            if (board[r][c] == 0) {
+                check = false;
+                break;
+            }
         }
-        if(!check){
-            x--;
+        if (!check) {
+            r--;
             continue;
         }
         score++;
-        for(int xx=x; xx>=5; xx--){
-            for(int y=0; y<4; y++){
-                board[y][xx] = board[y][xx-1];
+        for (int c=0; c<=3; c++) {
+            for (int rr=r; rr>=4; rr--) {
+                board[rr][c] = board[rr-1][c];
             }
-        }
-        for(int y=0; y<4; y++){
-            board[y][4] = 0;
         }
     }
 }
 
-void check_green_board(){
-    int y = 9;
-    bool check = true;
-    while(y >= 6){
-        check = true;
-        for(int x=0; x<4; x++){
-            if(!board[y][x]) check = false;
+void check_blue_board() {
+    int c=9;
+    while (c > 5) {
+        bool check = true;
+        for (int r=0; r<=3; r++) {
+            if (board[r][c] == 0) {
+                check = false;
+                break;
+            }
         }
-        if(!check){
-            y--;
+        if (!check) {
+            c--;
             continue;
         }
         score++;
-        for(int yy=y; yy>=5; yy--){
-            for(int x=0; x<4; x++){
-                board[yy][x] = board[yy-1][x];
+        for (int r=0; r<=3; r++) {
+            for (int cc=c; cc>=4; cc--) {
+                board[r][cc] = board[r][cc-1];
             }
-        }
-        for(int x=0; x<4; x++){
-            board[4][x] = 0;
         }
     }
 }
 
-void check_sky_blue_board(){
-    int check = 0;
-    for(int x=5; x>=4; x--){
-        for(int y=0; y<4; y++){
-            if(board[y][x]){
-                check++;
+void check_light_green_board() {
+    int n = 0;
+    for (int r=4; r<=5; r++) {
+        for (int c=0; c<=3; c++) {
+            if (board[r][c]) {
+                n ++;
                 break;
             }
         }
     }
-    switch(check){
-        case 0:
-            break;
-        case 1:
-            for(int x=9; x>=6; x--){
-                for(int y=0; y<4; y++){
-                    board[y][x] = board[y][x-1];
-                }
-            }
-            for(int y=0; y<4; y++){
-                board[y][5] = 0;
-            }
-            break;
-        case 2:
-            for(int x=9; x>=5; x--){
-                for(int y=0; y<4; y++){
-                    board[y][x] = board[y][x-2];
-                }
-            }
-            for(int x=4; x<=5; x++){
-                for(int y=0; y<4; y++){
-                    board[y][x] = 0;
-                }
-            }
-            break;
+    
+    if (n == 0) return;
+    for (int c=0; c<=3; c++) {
+        for (int r=9; r>=4; r--) {
+            board[r][c] = board[r-n][c];
+        }
     }
 }
 
-void check_light_green_board(){
-    int check = 0;
-    for(int y=5; y>=4; y--){
-        for(int x=0; x<4; x++){
-            if(board[y][x]){
-                check++;
+void check_light_blue_board() {
+    int n = 0;
+    for (int c=4; c<=5; c++) {
+        for (int r=0; r<=3; r++) {
+            if (board[r][c]) {
+                n ++;
                 break;
             }
         }
     }
-    switch(check){
-        case 0:
-            break;
-        case 1:
-            for(int y=9; y>=6; y--){
-                for(int x=0; x<4; x++){
-                    board[y][x] = board[y-1][x];
-                }
-            }
-            for(int x=0; x<4; x++){
-                board[5][x] = 0;
-            }
-            break;
-        case 2:
-            for(int y=9; y>=5; y--){
-                for(int x=0; x<4; x++){
-                    board[y][x] = board[y-2][x];
-                }
-            }
-            for(int y=4; y<=5; y++){
-                for(int x=0; x<4; x++){
-                    board[y][x] = 0;
-                }
-            }
-            break;
+    
+    if (n == 0) return;
+    for (int r=0; r<=3; r++) {
+        for (int c=9; c>=4; c--) {
+            board[r][c] = board[r][c-n];
+        }
     }
 }
 
-void monominodomino(){
-    for(int i=0; i<blocks.size(); i++){
-        b = blocks[i];
-        //put block
-        switch(b.t){
-            case 1:
-                put_block1(b.x, b.y);
-                break;
-            case 2:
-                put_block2(b.x, b.y);
-                break;
-            case 3:
-                put_block3(b.x, b.y);
-                break;
-        }
-        //check board
-        check_blue_board();
+void monominodomino() {
+    for (int i=0; i<N; i++) {
+        int t = blocks[i].first;
+        int r = blocks[i].second.first;
+        int c = blocks[i].second.second;
+        
+        put_block(t, r, c);
+        
         check_green_board();
-        check_sky_blue_board();
+        
+        check_blue_board();
+        
         check_light_green_board();
+        
+        check_light_blue_board();
     }
 }
 
-int get_blocks(){
-    //get blocks on blue block
-    for(int y=0; y<4; y++){
-        for(int x=6; x<=9; x++){
-            blue_blocks += board[y][x];
+int get_blocks() {
+    int cnt = 0;
+    for (int r=0; r<=9; r++) {
+        for (int c=0; c<=9; c++) {
+            cnt += board[r][c];
         }
     }
-    //get blocks on green block
-    for(int y=6; y<=9; y++){
-        for(int x=0; x<4; x++){
-            green_blocks += board[y][x];
-        }
-    }
-    return blue_blocks+green_blocks;
+    
+    return cnt;
 }
 
 int main(int argc, const char * argv[]) {
     cin >> N;
-    for(int i=0; i<N; i++){
-        cin >> b.t >> b.y >> b.x;
-        blocks.push_back(b);
+    for (int i=0; i<N; i++) {
+        int t, x, y;
+        cin >> t >> x >> y;
+        blocks.push_back({t, {x, y}});
     }
+    
     monominodomino();
     cout << score << endl;
     cout << get_blocks() << endl;
+    
     return 0;
 }
